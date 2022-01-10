d11207c8c2823e6d9931882ec75f7463b1d0acd5
luiggy macias <ferrinluiggy@gmail.com>
2022-01-10 16:08:09 -0500

    gfgf

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,20 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script, { ScriptProps } from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload" // controls when the script is loaded
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        } // callback when the script is loaded
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -12,3 +24,14 @@ export default function FirstPost() {
     </>
   );
 }
+
+// import Image from 'next/image'
+
+// const YourComponent = () => (
+//   <Image
+//     src="/images/profile.jpg" // Route of the image file
+//     height={144} // Desired size with correct aspect ratio
+//     width={144} // Desired size with correct aspect ratio
+//     alt="Your Name"
+//   />
+// )
