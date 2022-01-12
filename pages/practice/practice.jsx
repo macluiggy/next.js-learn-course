4f45fdf71cea08dd1e318f1318dc9ce8db6e0a24
luiggy macias <ferrinluiggy@gmail.com>
2022-01-12 14:42:15 -0500

    learning about pre-rendering

---
+++ pages/practice/practice.jsx
@@ -0,0 +1,11 @@
+import Alert from "../../components/Alert/index.jsx";
+
+export default function practice() {
+  return (
+    <div>
+      <Alert>
+        <h1>hello</h1>
+      </Alert>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.jsx",
+    "./components/**/*.jsx",
+  ],
+  // ...
+};
