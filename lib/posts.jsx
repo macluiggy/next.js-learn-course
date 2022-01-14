692c194972bc65e56a7d563e68778199db780fed
luiggy macias <ferrinluiggy@gmail.com>
2022-01-14 15:04:29 -0500

    learning how to use getStaticPath with getStaticProps for dynamics routes
    hello

--- lib/posts.jsx
+++ lib/posts.jsx
@@ -34,3 +34,48 @@ export function getSortedPostsData() {
     }
   });
 }
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory); // this method returns an array of file names in the posts directory
+  console.log(fileNames);
+  //this is the array that must be inside the getStaticPaths returned object
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  // Read markdown file as string
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // // Use remark to convert markdown into HTML string
+  // const processedContent = matterResult.content;
+
+  // Combine the data with the id
+  return {
+    id,
+    // content: processedContent,
+    ...matterResult.data,
+  };
+}
